ff343d6f418c41e14cf79be8cb18855cda2a6ec4
dantzK <120504755+dantezzK@users.noreply.github.com>
2022-12-29 14:46:32 -0300

    att
    carrossel de produtos

--- Produtos/Enfardadeira-vertical/enfardadeira-vertical.js
+++ Produtos/Enfardadeira-vertical/enfardadeira-vertical.js
@@ -1,6 +1,35 @@
+let span = document.getElementsByTagName('span');
+	let product = document.getElementsByClassName('product')
+	let product_page = Math.ceil(product.length/4);
+	let l = 5;
+	let movePer = 25.34;
+	let maxMove = 203;
+	// mobile_view	
+	let mob_view = window.matchMedia("(max-width: 768px)");
+	if (mob_view.matches)
+	 {
+	 	movePer = 50.36;
+	 	maxMove = 504;
+	 }
 
-$(document).ready(function(){
-    $('.js-slider').slick({
-    
-    });
-  });
+     let right_mover = ()=>{
+		l = l + movePer;
+		if (product == 1){l = 0; }
+		for(const i of product)
+		{
+			if (l > maxMove){l = l - movePer;}
+			i.style.left = '-' + l + '%';
+		}
+
+	}
+	let left_mover = ()=>{
+		l = l - movePer;
+		if (l<=0){l = 0;}
+		for(const i of product){
+			if (product_page>1){
+				i.style.left = '-' + l + '%';
+			}
+		}
+	}
+	span[2].onclick = ()=>{right_mover();}
+	span[1].onclick = ()=>{left_mover();}
