e91936ad25a35d3cbca126c6ceb70619bbe10951
Scs Digital Business <rezendeevil9000@gmail.com>
2023-02-22 14:45:04 -0300

    att
    att

---
+++ Contato/py.py
@@ -0,0 +1,23 @@
+ 
+
+print ("Olá Mundo sou o Swepper, estou ansioso para podermos trabalhar juntos! Para começar preciso que digite o seu nome: ") 
+nome= input()
+print ("Olá,", nome, "Estamos ansiosos para comerçar a  sua entrevista!") 
+print("Por Favor Digite sua idade:") 
+idade= input() 
+print ("Só mais um segundo... estamos guardando seus dados..." )  
+print ("Insira sua data de nascimento:") 
+data= input()  
+print ("Muito Obrigado, agora vamos precisar que nos forneça o ano atual que estamos: ")  
+anoatual=input() 
+print ("Para ter mais informações  sobre você precisamos de mais algumas informações, por favor nos diga de qual estado você pertence: ") 
+estado= input() 
+print("Okay,", nome, "agora precisamos de mais alguns dados, porém desta vez, precisamos de dados relacionados a sua economia,")  
+print("Qual o seu salário anual ?")  
+sal = input() 
+salarioa= sal *12  
+print("Seu Salário Anual é:", salarioa) 
+
+
+
+ 
